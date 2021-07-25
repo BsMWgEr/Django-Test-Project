5f335635e34571af4fc7519911aba235a69d07dd
j q <BsMWgErTYR4LEDR9@protonmail.ch>
2021-07-25 18:45:53 -0400

    Testing push on pycharm again

--- Project/views.py
+++ Project/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 
 def home_page(request):
     my_title = 'Hello there...'
-    return render(request, 'about.html', {"title": my_title})
+    context = {"title": "my title"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    return render(request, 'home.html', context)
+
+
+def about_page(request):
+    return render(request, "about.html", {"title": "About"})
+
+
+def contact_page(request):
+    return render(request, )
