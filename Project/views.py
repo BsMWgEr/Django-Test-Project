eb81a7d55a42c6c29f4b3b1ca0d932a7a4879fa2
j q <BsMWgErTYR4LEDR9@protonmail.ch>
2021-07-26 19:15:28 -0400

    Authenticates user on home.html and displays text

--- Project/views.py
+++ Project/views.py
@@ -1,18 +1,28 @@
-
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
     my_title = 'Hello there...'
-    context = {"title": "my title"}
-    if request.user.is_authenticated:
-        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    context = {"title": "my title", "my_list": [1, 2, 3, 4, 5]}
+    
     return render(request, 'home.html', context)
 
 
 def about_page(request):
-    return render(request, "about.html", {"title": "About"})
+    context = {'about.html': 'About'}
+    return render(request, "about.html", context)
 
 
 def contact_page(request):
-    return render(request, )
+    context = {'title': 'Contact'}
+    return render(request, 'contact.html', context)
+
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "home.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
