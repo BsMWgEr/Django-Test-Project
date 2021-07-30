e05952e5ba7561ff479c2c221ba38137305a8cf6
j q <BsMWgErTYR4LEDR9@protonmail.ch>
2021-07-30 17:16:36 -0400

    general updates I think?

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 # Create your views here.
 
 
-def blog_post_detail_page(request, post_id):
-    obj = BlogPost.objects.get(id=post_id)
+def blog_post_detail_page(request, slug):
+    print("DJANGO SAYS", request.method, request.path, request.user)
+    # obj = BlogPost.objects.get(slug=slug)
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #    raise Http404
+    # obj = queryset.first()
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
+
+
+def blog_post_list_view(request):
+    template_name = "blog_post_list.html"
+    context = {'object_list': []}
+    return render(request, template_name, context)
+
+
+def blog_post_create_view(request):
+    return
+
+
+def blog_post_retrieve_view(request):
+    return
+
+
+def blog_post_update_view(request):
+    return
+
+
+def blog_post_delete_view(request):
+    return
+
